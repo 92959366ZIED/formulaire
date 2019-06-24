6b2c866a8fa493b924f5a4dd9863f9877df01996
92959366ZIED <hhamdiziyed990@gmail.com>
2019-06-24 11:39:27 +0200

    2commit

--- main.js
+++ main.js
@@ -1,102 +1,81 @@
 function  a(){
-    var nom = document.getElementById('nom').value;
+    var nom = document.forms.myform.elements.nom.value;
     var Prenom = document.getElementById('Prenom').value;
-    var Address = document.getElementById('address').value;
+    var Address = document.getElementById('Address').value;
     var mail = document.getElementById('email').value;
     var password = document.getElementById('password').value;
     var Comments = document.getElementById('Comments').value;
-    let validation= true
-    if( nom === "")
+  
+        if(nom === "")
         {
             alert("le nom est vide");
-            ok = false
-        }
-        else
-        
-        if (nom.length < 3)
-        {
-            alert("min de nom 3 est caractère"); validation = false
-        } 
-    
-
-    if (Prenom === "")
-    {
-        alert("le Prenom est vide"); validation = false;
-    }
-        else
-        if (Prenom.length < 3)
-        {  
-            alert("min de Prenom 3 est caractère"); validation = false
-        }
-        
-
-    if (Address === "")
-    {
-        alert("le Address est vide"); validation = false
-    }
-        else
-        if (Address.length < 8)
-        {
-            alert("min de Address 8 est caractère"); validation = false;
-       
-        }
-
-    if (mail === "")
-    {
-        alert("le mail est vide"); ok = false
-    }
-        else
-        {
-        if (mail.length < 3)
-        {
-            alert("min de mail 3 est caractère"); validation = false
-        }
-            else
-            if (!(mail.includes("@")))
-            {
-                alert("obligatoire de @"); validation = false;
-            }
-        }
-
-    if (password === "")
-    {
-        alert("le password est vide"); validation = false;
-    }
-        else
-        {
-            if (password.length < 8)
-            {
-                alert("min de password 8 est caractère"); validation = false
-            }
            
         }
-        
-
-    if (Comments === "")
-    {
-        alert("le Comments est vide"); validation = false;
-    } 
+        else if (nom.length < 3)
+        {
+            alert("min de nom 3 est caractère");
+        } 
+        else if (Prenom === "")
+        {
+            alert("le Prenom est vide"); 
+        }
+        else if (Prenom.length < 3)
+        {  
+            alert("min de Prenom 3 est caractère");
+        }
+        else if (Address === "")
+        {
+            alert("le Address est vide"); 
+        }
+        else if (Address.length < 8)
+        {
+            alert("min de Address 8 est caractère");
+       
+        }
+        else if (mail === "")
+        {
+            alert("le mail est vide");
+        }
+        else if (mail.length < 3)
+        {
+            alert("min de mail 3 est caractère"); 
+        }
+        else if (!(mail.includes("@")))
+        {
+            alert("obligatoire de @"); 
+        }
+        else if (password === "")
+        {
+            alert("le password est vide"); 
+        }
+        else if (password.length < 8)
+        {
+            alert("min de password 8 est caractère"); 
+        }
+        else if (Comments === "")
+        {
+            alert("le Comments est vide"); 
+        } 
+        else if (Comments.length < 8)
+        { 
+            alert("min de Comments 8 est caractère"); 
+        }
         else
         {
-            if (Comments.length < 8)
-            { 
-                alert("min de Comments 8 est caractère"); validation = false
-            }
-        
-
+            alert('validée')
         }
    
 }
-function ()
+// function ()
 
-if (validation === true) {
-    document.getElementById('nom').value = "";
-    document.getElementById('Prenom').value = "";
-    document.getElementById('Address').value = "";
-    document.getElementById('email').value = "";
-    document.getElementById('password').value = "";
-    document.getElementById('Comments').value = "";
-    alert("validation") 
+// if (validation === true) {
+//     document.getElementById('nom').value = "";
+//     document.getElementById('Prenom').value = "";
+//     document.getElementById('Address').value = "";
+//     document.getElementById('email').value = "";
+//     document.getElementById('password').value = "";
+//     document.getElementById('Comments').value = "";
+//     alert("validation") 
 
 
-}
+// }
